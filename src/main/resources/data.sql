e971b525a5f62dc7ab0317d7d23cbc49b36b8e2d
Мария <manjasha_lee@mail.ru>
2021-03-26 14:52:41 +0600

    Созданы и заполнены таблицы БД

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,23 @@
-INSERT INTO House (id, version, address) VALUES (1, 0, 'ул.Цюрупы, 16');
+INSERT INTO Organization (version, name, full_name, inn, kpp, address, phone, is_active) VALUES (0, 'Oazis', 'ООО GreenLine', '1234567890', '123456789', 'ул.Толстого, д.45, кв.10', '87054322345','1' );
+INSERT INTO Organization (version, name, full_name, inn, kpp, address, phone, is_active) VALUES (0, 'Содружество', 'ОАО Содружество', '1432567890', '125436789', 'ул.Пушкина, д.25, кв.3', '87754223355','1' );
+INSERT INTO Organization (version, name, full_name, inn, kpp, address, phone, is_active) VALUES (0, 'Сбор', 'ЗАО Сбор', '3214567890', '353456789', 'ул.Королева, д.6, кв.5', '87774323245','1' );
+INSERT INTO Organization (version, name, full_name, inn, kpp, address, phone, is_active) VALUES (0, 'Фараон', 'ОАО Фараон', '4321567890', '543216789', 'пр.Ленина, д.108', '87056677345','1' );
+INSERT INTO Organization (version, name, full_name, inn, kpp, address, phone, is_active) VALUES (0, 'Планета Спорта', 'ООО Планета Спорта', '9898567890', '196216789', 'ул.Садовая, д.19', '87776677878','1' );
 
-INSERT INTO House (id, version, address) VALUES (2, 0, 'ул.Лунина, 7');
+INSERT INTO Office (version, org_id, name, address, is_active) VALUES (0, 1, 'Oazis1', 'ул.Горького д.115, кв.4', '1');
+INSERT INTO Office (version, org_id, name, address, is_active) VALUES (0, 1, 'Oazis2', 'ул.Тарана д.85, кв.1', '1');
+INSERT INTO Office (version, org_id, name, address, is_active) VALUES (0, 3, 'Сбор3', 'ул.Гагарина д.56, кв.5', '1');
+INSERT INTO Office (version, org_id, name, address, is_active) VALUES (0, 5, 'Level Up', 'ул.Гоголя д.96, кв.2', '1');
+INSERT INTO Office (version, org_id, name, address, is_active) VALUES (0, 5, 'Титан', 'ул.Победы д.145, кв.1', '1');
 
-INSERT INTO Person (id, version, first_name, age) VALUES (1, 0, 'Пётр', 20);
+INSERT INTO User (version, office_id, first_name, last_name, middle_name, position, phone, is_identified) VALUES (0, 2, 'Ирина', 'Цветкова', 'Федоровна', 'флорист', '87779564565', '1');
+INSERT INTO User (version, office_id, first_name, last_name, middle_name, position, phone, is_identified) VALUES (0, 3, 'Петр', 'Иванов', 'Петрович', 'менеджер', '87776794565', '1');
+INSERT INTO User (version, office_id, first_name, last_name, middle_name, position, phone, is_identified) VALUES (0, 5, 'Иван', 'Петров', 'Иванович', 'тренер', '87759544577', '1');
+INSERT INTO User (version, office_id, first_name, last_name, middle_name, position, phone, is_identified) VALUES (0, 4, 'Татьяна', 'Лукина', 'Сергеевна', 'администратор', '87779598165', '1');
+INSERT INTO User (version, office_id, first_name, last_name, middle_name, position, phone, is_identified) VALUES (0, 3, 'Игорь', 'Сидоров', 'Сергеевич', 'экономист', '87788754511', '1');
 
-INSERT INTO Person (id, version, first_name, age) VALUES (2, 0, 'John', 25);
-
-INSERT INTO Person_House (person_id, house_id) VALUES (1, 1);
-
-INSERT INTO Person_House (person_id, house_id) VALUES (1, 2);
+INSERT INTO Doc (user_id, version, doc_name, doc_number, doc_date, citizenship_name, citizenship_code, doc_code) VALUES (1, 0, 'Паспорт гражданина Российской Федерации', '1234567890', '2010-05-04', 'РФ', '643', '21');
+INSERT INTO Doc (user_id, version, doc_name, doc_number, doc_date, citizenship_name, citizenship_code, doc_code) VALUES (2, 0, 'Паспорт гражданина Российской Федерации', '3334567987', '2009-10-25', 'РФ', '643', '21');
+INSERT INTO Doc (user_id, version, doc_name, doc_number, doc_date, citizenship_name, citizenship_code, doc_code) VALUES (3, 0, 'Паспорт гражданина Российской Федерации', '4534567456', '2012-12-12', 'РФ', '643', '21');
+INSERT INTO Doc (user_id, version, doc_name, doc_number, doc_date, citizenship_name, citizenship_code, doc_code) VALUES (4, 0, 'Паспорт гражданина Российской Федерации', '7634567136', '2020-04-10', 'РФ', '643', '21');
+INSERT INTO Doc (user_id, version, doc_name, doc_number, doc_date, citizenship_name, citizenship_code, doc_code) VALUES (5, 0, 'Паспорт гражданина Российской Федерации', '8734567875', '2008-08-01', 'РФ', '643', '21');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,56 @@
-CREATE TABLE IF NOT EXISTS Person (
+CREATE TABLE IF NOT EXISTS Organization (
     id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    version    INTEGER NOT NULL     COMMENT 'Служебное поле hibernate',
-    first_name VARCHAR(50) NOT NULL COMMENT 'Имя',
-    age        INTEGER  NOT NULL    COMMENT 'Возраст'
+    version    INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
+    name       VARCHAR(20) NOT NULL COMMENT 'Имя',
+    full_name  VARCHAR(50) NOT NULL COMMENT 'Полное имя',
+    inn        VARCHAR(10) NOT NULL COMMENT 'ИHH',
+    kpp        VARCHAR(9)  NOT NULL COMMENT 'КПП',
+    address    VARCHAR(50) NOT NULL COMMENT 'Адрес',
+    phone      VARCHAR(11)          COMMENT 'Телефон',
+    is_active  VARCHAR(1)           COMMENT 'Активен'
 );
-COMMENT ON TABLE Person IS 'Человек';
+COMMENT ON TABLE Organization IS 'Организация';
 
-CREATE TABLE IF NOT EXISTS House (
+CREATE TABLE IF NOT EXISTS Office (
     id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-    version    INTEGER NOT NULL     COMMENT 'Служебное поле hibernate',
-    address    VARCHAR(50) NOT NULL COMMENT 'Адрес'
+    version    INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
+    org_id     INTEGER     NOT NULL COMMENT 'Уникальный идентификатор организации',
+    name       VARCHAR(20) NOT NULL COMMENT 'Имя',
+    address    VARCHAR(50) NOT NULL COMMENT 'Адрес',
+    is_active  VARCHAR(1)           COMMENT 'Активен'
 );
-COMMENT ON TABLE House IS 'Дом';
+COMMENT ON TABLE Office IS 'Офис';
 
-CREATE TABLE IF NOT EXISTS Person_House (
-    person_id   INTEGER  NOT NULL COMMENT 'Уникальный идентификатор человека',
-    house_id    INTEGER  NOT NULL COMMENT 'Уникальный идентификатор дома',
-
-    PRIMARY KEY (person_id, house_id)
+CREATE TABLE IF NOT EXISTS User (
+    id          INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version     INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
+    office_id   INTEGER     NOT NULL COMMENT 'Уникальный идентификатор офиса',
+    first_name  VARCHAR(20) NOT NULL COMMENT 'Имя',
+    last_name   VARCHAR(30)          COMMENT 'Фамилия',
+    middle_name VARCHAR(25)          COMMENT 'Отчество',
+    position   VARCHAR(20) NOT NULL COMMENT 'Должность',
+    phone       VARCHAR(11)          COMMENT 'Телефон',
+    is_identified VARCHAR(1)         COMMENT 'Идентифицирован'
 );
-COMMENT ON TABLE Person_House IS 'join-таблица для связи человека и дома';
+COMMENT ON TABLE User IS 'Пользователь';
 
-CREATE INDEX IX_Person_House_Id ON Person_House (house_id);
-ALTER TABLE Person_House ADD FOREIGN KEY (house_id) REFERENCES House(id);
+CREATE TABLE IF NOT EXISTS Doc (
+    user_id     INTEGER     NOT NULL COMMENT 'Уникальный идентификатор пользователя',
+    version     INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
+    doc_name    VARCHAR(50)          COMMENT 'Наименование',
+    doc_number  VARCHAR(20)          COMMENT 'Номер',
+    doc_date    DATE                 COMMENT 'Дата выдачи',
+    citizenship_name VARCHAR(50)     COMMENT 'Гражданство',
+    citizenship_code VARCHAR(3)      COMMENT 'Код гражданства',
+    doc_code    VARCHAR(2)           COMMENT 'Код документа'
+);
+COMMENT ON TABLE Doc IS 'Документ';
 
-CREATE INDEX IX_House_Person_Id ON Person_House (person_id);
-ALTER TABLE Person_House ADD FOREIGN KEY (person_id) REFERENCES Person(id);
+CREATE INDEX IX_Organization_id ON Office (org_id);
+ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization(id);
+
+CREATE INDEX IX_Office_id ON User (office_id);
+ALTER TABLE User ADD FOREIGN KEY (office_id) REFERENCES Office(id);
+
+CREATE INDEX IX_Doc_id ON Doc (user_id);
+ALTER TABLE Doc ADD FOREIGN KEY (user_id) REFERENCES User(id);
